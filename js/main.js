ea6a3c88de55b8d0c4fd99e7bdb13709acf92643
tisma-j <matis.jouet@gmail.com>
2018-12-20 17:07:27 +0100

    work anim

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
+
 var previousProject = document.querySelector('.current');
 
 var nbrs = document.querySelectorAll('.nbr-work');
 
+
 nbrs.forEach(function(n) {
-  n.addEventListener('click', function() {
+  n.addEventListener('click', function(event) {
+    nbrs.forEach(function(elt) {
+      elt.classList.remove('active');
+    });
+    //_removeClasses();
+    event.target.classList.add('active');
     previousProject.classList.remove('current');
     var targetProject = document.querySelector(n.dataset.target);
     targetProject.classList.add('current');
